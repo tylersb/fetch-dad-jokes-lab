749804d6ee92a54a37fc46cc2969d126fcbac463
tylersb <tylersb@gmail.com>
2022-11-22 15:53:58 -0800

    updates the page to fetch an image of a joke, and only fetch it on button press + fetch new joke on button press
    Signed-off-by: tylersb <tylersb@gmail.com>

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 // define the url to use
 const requestURL = 'https://icanhazdadjoke.com/'
 
-// make sure the js doesn't start until the document loads
 document.addEventListener('DOMContentLoaded', () => {
-  // request data from the chosen API
-  fetch(requestURL, {
-    // set the header for the data type we want to query from the API
-    headers: {
-      'Accept': 'application/json'
-    }}) // <--- This broke before realizing I needed 2 } intead of 1 here
-    // parse the returned value into a json object we can interact with
-  .then((responseData) => responseData.json())
-  // interact with the parsed json
-  .then((peopleData) => {
-    console.log(peopleData)
-    // define the html element to change
-    const joke = document.querySelector('#joke')
-    // set the inner text of the HTML element to the portion of the API response that we select
-    joke.innerText = peopleData.joke
-})
+  const button = document.querySelector('#button')
+  button.addEventListener('click', (e) => {
+    fetch(requestURL, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+      .then((responseData) => responseData.json())
+      .then((peopleData) => {
+        const joke = document.querySelector('#joke')
+        joke.innerHTML = '<img src="https://icanhazdadjoke.com/j/' + peopleData.id + '.png" />'
+      })
+  })
 })
